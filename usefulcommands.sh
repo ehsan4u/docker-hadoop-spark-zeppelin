5521b9990e6af5e8794a3f68199976f46c953b49
Ehsan <ehsan4u@gmail.com>
2021-12-05 09:50:52 -0500

    initialization

---
+++ usefulcommands.sh
@@ -0,0 +1,35 @@
+# Useful commands
+
+# if powershell - switch to bash
+
+bash
+port=8091
+netstat -anp | grep ${port}
+
+
+conda env list conda create -n xxx conda remove -n xxx
+
+I have tried many solutions, but they turn out to be useless:
+
+conda init powershell
+restart powershell
+conda activate 
+
+
+conda update -n base conda and redo 1
+
+conda install -n root -c pscondaenvs pscondaenvs 
+Set-ExecutionPolicy RemoteSigned
+Run activate base
+
+
+docker-compose up -d
+
+
+#  nifi
+
+https://github.com/CribberSix/docker-compose
+
+# imp solutions
+
+https://deparkes.co.uk/2019/11/16/nifi-version-control-with-nifi-registry/
